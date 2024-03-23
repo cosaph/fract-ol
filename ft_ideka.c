17a06ea89cfc4568bfea2779b8fc37d95210800e
Coralie Cottet <coralie.cottet@eleve.ensai.fr>
2024-03-23 17:53:03 +0100

    problem de norme sur ideka

--- ft_ideka.c
+++ ft_ideka.c
@@ -6,29 +6,34 @@
 /*   By: ccottet <ccottet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 11:32:06 by ccottet           #+#    #+#             */
-/*   Updated: 2024/03/23 17:24:06 by ccottet          ###   ########.fr       */
+/*   Updated: 2024/03/23 17:52:28 by ccottet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_fractol.h"
 
-void	ikedaattractor(t_fractal *ikeda)
-{
-	int	i;
+void	ikedaattractor(t_fractal *ikeda){
+	int		i;
+	double	x;
+	double	y;
 
 	i = 0;
-	while (i < ikeda->iterations)
-	{
-		ikeda->t = 0.4 - 6.0 / (1.0 + 0.1 * 0.1 + 0.0 * 0.0);
-		ikeda->x_new = 1.0 + ikeda->u
-			* (0.1 * cos(ikeda->t) - 0.0 * sin(ikeda->t));
-		ikeda->y_new = ikeda->u * (0.1 * sin(ikeda->t) + 0.0 * cos(ikeda->t));
-		ikeda->pixel_x = ikeda->width / 2 + ikeda->x_new * ikeda->width / 5;
-		ikeda->pixel_y = ikeda->height / 2 + ikeda->y_new * ikeda->height / 5;
-		if (ikeda->pixel_x >= 0 && ikeda->pixel_x < ikeda->width
-			&& ikeda->pixel_y >= 0 && ikeda->pixel_y < ikeda->height)
-			my_mlx_pixel_put(ikeda, ikeda->pixel_x,
-				ikeda->pixel_y, ikeda->color * i);
+	x = 0.1;
+	y = 0.0;
+	while (i < ikeda->iterations) {
+		double t = 0.4 - 6.0 / (1.0 + x * x + y * y);
+		double x_new = 1.0 + ikeda->u * (x * cos(t) - y * sin(t));
+		double y_new = ikeda->u * (x * sin(t) + y * cos(t));
+
+		x = x_new;
+		y = y_new;
+
+		int pixel_x = ikeda->width / 2 + x * ikeda->width / 5;
+		int pixel_y = ikeda->height / 2 + y * ikeda->height / 5;
+
+		if (pixel_x >= 0 && pixel_x < ikeda->width && pixel_y >= 0 && pixel_y < ikeda->height) {
+			my_mlx_pixel_put(ikeda, pixel_x, pixel_y, ikeda->color * i);
+		}
 		i++;
 	}
 }
